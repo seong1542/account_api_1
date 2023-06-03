2db9b1d958dcff4d53804313043f01a91fb4ecc0
cheoljinpark <menu34@paran.com>
2023-06-03 17:51:06 +0900

    status entity 추가

--- src/main/java/com/nhnacademy/account_api_1/entity/Status.java
+++ src/main/java/com/nhnacademy/account_api_1/entity/Status.java
@@ -1,11 +1,29 @@
 package com.nhnacademy.account_api_1.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
-@Getter
-@ToString
-public enum Status {
+import javax.persistence.*;
+import java.util.List;
 
-    JOINED, DORMANT, WITHDRAWAL
+
+@Entity
+@Getter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class Status {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "status_id")
+    private Long statusId;
+
+    @Column(name = "status_name")
+    private String statusName;
+
+    @OneToMany
+    @JoinColumn(name = "index_id")
+    private List<User> users;
 }
--- src/main/java/com/nhnacademy/account_api_1/entity/User.java
+++ src/main/java/com/nhnacademy/account_api_1/entity/User.java
@@ -26,6 +26,7 @@ public class User {
 
     private String email;
 
-    @Enumerated(value = EnumType.STRING)
+    @ManyToOne(fetch = FetchType.LAZY)
     private Status status;
+
 }
