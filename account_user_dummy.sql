ff2857b5a23f40bc619e469db6b7a8ea8229a2d4
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-06-03 15:39:26 +0900

    users_dummyfile
    Users table dummyfile upload

---
+++ account_user_dummy.sql
@@ -0,0 +1,8 @@
+insert into Users VALUES ("user1", "12345", "user1@academy.com","가입");
+insert into Users VALUES ("user2", "12345", "user2@gmail.com","가입");
+insert into Users VALUES ("user3", "12345", "user3@naver.com","휴면");
+insert into Users VALUES ("user4", "12345", "user4@academy.com","휴면");
+insert into Users VALUES ("user5", "12345", "user5@academy.com","가입");
+insert into Users VALUES ("user6", "12345", "user6@gmail.com","가입");
+insert into Users VALUES ("user7", "12345", "user7@academy.com","탈퇴");
+insert into Users VALUES ("user8", "12345", "user8@naver.com","가입");
