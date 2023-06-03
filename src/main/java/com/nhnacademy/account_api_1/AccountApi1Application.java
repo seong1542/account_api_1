3655560aafbccb87bbe4813fae88a32762d12f10
유승연 <seong1542@naver.com>
2023-06-03 14:28:32 +0900

    test

---
+++ src/main/java/com/nhnacademy/account_api_1/AccountApi1Application.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.account_api_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountApi1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/nhnacademy/account_api_1/P.java
@@ -0,0 +1,5 @@
+package com.nhnacademy.account_api_1;
+
+public class P {
+
+}
