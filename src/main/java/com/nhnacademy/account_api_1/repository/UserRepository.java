68eb7dba1c245e158cc2e8cc24200d036cd33db2
유승연 <seong1542@naver.com>
2023-06-06 21:48:28 +0900

    modified-statusEntity, add statusCode

--- src/main/java/com/nhnacademy/account_api_1/repository/UserRepository.java
+++ src/main/java/com/nhnacademy/account_api_1/repository/UserRepository.java
@@ -11,5 +11,5 @@ import java.util.Optional;
 
 
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom{
-    Boolean existsByUserId(String userId);
+    boolean existsByUserId(String userId);
 }
