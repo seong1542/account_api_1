50d6abaa1ac43506475abd1976c2eeb4cc2d3103
유승연 <seong1542@naver.com>
2023-06-06 20:38:23 +0900

    modified-ControllerTestClass & delete Request Setter

--- src/main/java/com/nhnacademy/account_api_1/request/UserRequest.java
+++ src/main/java/com/nhnacademy/account_api_1/request/UserRequest.java
@@ -1,15 +1,11 @@
 package com.nhnacademy.account_api_1.request;
 
 import com.nhnacademy.account_api_1.entity.Status;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@Setter
 @NoArgsConstructor
 public class UserRequest {
 
@@ -17,6 +13,13 @@ public class UserRequest {
     private String userId;
     @NotBlank(message = "비밀번호를 입력하세요")
     private String password;
+
+    @NotBlank(message = "이메일을 입력하세요")
     private String email;
 
+    public UserRequest(String userId, String password, String email){
+        this.userId = userId;
+        this.password = password;
+        this.email =email;
+    }
 }
