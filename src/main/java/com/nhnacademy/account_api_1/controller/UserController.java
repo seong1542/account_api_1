94dc53645d83e7aa6673a4a6853a9910beeb52d0
seong1542 <66362713+seong1542@users.noreply.github.com>
2023-12-08 16:36:21 +0900

    log 취소

--- src/main/java/com/nhnacademy/account_api_1/controller/UserController.java
+++ src/main/java/com/nhnacademy/account_api_1/controller/UserController.java
@@ -56,7 +56,7 @@ public class UserController {
         if(bindingResult.hasErrors()){
             throw new ValidationException();
         }
-        log.debug("here?");
+
         userService.updateUser(id, userModify);
     }
 
